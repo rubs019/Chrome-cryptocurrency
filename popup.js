59bbdfa79eb417c0da0137448d4dcda4bf190a93
rubs019 <ruben.desert@eemi.com>
2018-01-17 21:58:27 +0100

    fix some details & remove data sample

--- popup.js
+++ popup.js
@@ -1,176 +1,3 @@
-let crypto = [
-    {
-        "id": "bitcoin", 
-        "name": "Bitcoin", 
-        "symbol": "BTC", 
-        "rank": "1", 
-        "price_usd": "12171.1", 
-        "price_btc": "1.0", 
-        "24h_volume_usd": "15767500000.0", 
-        "market_cap_usd": "204549782596", 
-        "available_supply": "16806187.0", 
-        "total_supply": "16806187.0", 
-        "max_supply": "21000000.0", 
-        "percent_change_1h": "1.43", 
-        "percent_change_24h": "-14.89", 
-        "percent_change_7d": "-18.2", 
-        "last_updated": "1516119863"
-    }, 
-    {
-        "id": "ethereum", 
-        "name": "Ethereum", 
-        "symbol": "ETH", 
-        "rank": "2", 
-        "price_usd": "1135.89", 
-        "price_btc": "0.0928739", 
-        "24h_volume_usd": "6702300000.0", 
-        "market_cap_usd": "110202619488", 
-        "available_supply": "97018743.0", 
-        "total_supply": "97018743.0", 
-        "max_supply": null, 
-        "percent_change_1h": "1.69", 
-        "percent_change_24h": "-14.54", 
-        "percent_change_7d": "-7.0", 
-        "last_updated": "1516120152"
-    }, 
-    {
-        "id": "ripple", 
-        "name": "Ripple", 
-        "symbol": "XRP", 
-        "rank": "3", 
-        "price_usd": "1.3989", 
-        "price_btc": "0.00011438", 
-        "24h_volume_usd": "4505430000.0", 
-        "market_cap_usd": "54192186878.0", 
-        "available_supply": "38739142811.0", 
-        "total_supply": "99993093880.0", 
-        "max_supply": "100000000000", 
-        "percent_change_1h": "1.23", 
-        "percent_change_24h": "-23.66", 
-        "percent_change_7d": "-38.59", 
-        "last_updated": "1516120141"
-    }, 
-    {
-        "id": "bitcoin-cash", 
-        "name": "Bitcoin Cash", 
-        "symbol": "BCH", 
-        "rank": "4", 
-        "price_usd": "2041.29", 
-        "price_btc": "0.166901", 
-        "24h_volume_usd": "1500940000.0", 
-        "market_cap_usd": "34528420350.0", 
-        "available_supply": "16915000.0", 
-        "total_supply": "16915000.0", 
-        "max_supply": "21000000.0", 
-        "percent_change_1h": "2.71", 
-        "percent_change_24h": "-17.11", 
-        "percent_change_7d": "-15.46", 
-        "last_updated": "1516120157"
-    }, 
-    {
-        "id": "cardano", 
-        "name": "Cardano", 
-        "symbol": "ADA", 
-        "rank": "5", 
-        "price_usd": "0.664026", 
-        "price_btc": "0.00005429", 
-        "24h_volume_usd": "1311180000.0", 
-        "market_cap_usd": "17216248941.0", 
-        "available_supply": "25927070538.0", 
-        "total_supply": "31112483745.0", 
-        "max_supply": "45000000000.0", 
-        "percent_change_1h": "2.1", 
-        "percent_change_24h": "-21.38", 
-        "percent_change_7d": "-22.45", 
-        "last_updated": "1516120160"
-    }, 
-    {
-        "id": "litecoin", 
-        "name": "Litecoin", 
-        "symbol": "LTC", 
-        "rank": "6", 
-        "price_usd": "206.09", 
-        "price_btc": "0.0168505", 
-        "24h_volume_usd": "1022840000.0", 
-        "market_cap_usd": "11290199237.0", 
-        "available_supply": "54782858.0", 
-        "total_supply": "54782858.0", 
-        "max_supply": "84000000.0", 
-        "percent_change_1h": "0.74", 
-        "percent_change_24h": "-14.32", 
-        "percent_change_7d": "-17.88", 
-        "last_updated": "1516120141"
-    }, 
-    {
-        "id": "nem", 
-        "name": "NEM", 
-        "symbol": "XEM", 
-        "rank": "7", 
-        "price_usd": "1.15055", 
-        "price_btc": "0.00009407", 
-        "24h_volume_usd": "153826000.0", 
-        "market_cap_usd": "10354949999.0", 
-        "available_supply": "8999999999.0", 
-        "total_supply": "8999999999.0", 
-        "max_supply": null, 
-        "percent_change_1h": "1.65", 
-        "percent_change_24h": "-20.82", 
-        "percent_change_7d": "-29.07", 
-        "last_updated": "1516120146"
-    }, 
-    {
-        "id": "neo", 
-        "name": "NEO", 
-        "symbol": "NEO", 
-        "rank": "8", 
-        "price_usd": "158.03", 
-        "price_btc": "0.012921", 
-        "24h_volume_usd": "1728790000.0", 
-        "market_cap_usd": "10271950000.0", 
-        "available_supply": "65000000.0", 
-        "total_supply": "100000000.0", 
-        "max_supply": null, 
-        "percent_change_1h": "3.63", 
-        "percent_change_24h": "-11.09", 
-        "percent_change_7d": "29.33", 
-        "last_updated": "1516120150"
-    }, 
-    {
-        "id": "stellar", 
-        "name": "Stellar", 
-        "symbol": "XLM", 
-        "rank": "9", 
-        "price_usd": "0.495505", 
-        "price_btc": "0.00004051", 
-        "24h_volume_usd": "372545000.0", 
-        "market_cap_usd": "8864965269.0", 
-        "available_supply": "17890768548.0", 
-        "total_supply": "103610058900", 
-        "max_supply": null, 
-        "percent_change_1h": "1.86", 
-        "percent_change_24h": "-22.34", 
-        "percent_change_7d": "-20.25", 
-        "last_updated": "1516120145"
-    }, 
-    {
-        "id": "iota", 
-        "name": "IOTA", 
-        "symbol": "MIOTA", 
-        "rank": "10", 
-        "price_usd": "3.05488", 
-        "price_btc": "0.00024978", 
-        "24h_volume_usd": "184404000.0", 
-        "market_cap_usd": "8491131471.0", 
-        "available_supply": "2779530283.0", 
-        "total_supply": "2779530283.0", 
-        "max_supply": "2779530283.0", 
-        "percent_change_1h": "1.46", 
-        "percent_change_24h": "-14.93", 
-        "percent_change_7d": "-16.49", 
-        "last_updated": "1516120155"
-    }
-]
-
 // This extension loads the saved background color for the current tab if one
 // exists. The user can select a new background color from the dropdown for the
 // current page, and it will be saved as part of the extension's isolated
@@ -181,12 +8,18 @@ let crypto = [
 // user devices.
 document.addEventListener('DOMContentLoaded', () => {
 
+    let limit = 10;
+    let convert = 'EUR';
     const settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://cryptocurrency-net.herokuapp.com/",
-      "method": "GET",
-      "headers": {}
+      'async': true,
+      'crossDomain': true,
+      'url': 'https://cryptocurrency-net.herokuapp.com/',
+      'method': 'GET',
+      'data': {
+        'limit' : limit,
+        'convert': convert
+      },
+      'headers': {}
     }
 
     $.ajax(settings).done((response) => {
